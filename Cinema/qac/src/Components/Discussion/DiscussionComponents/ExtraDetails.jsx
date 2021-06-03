f5f36241899fd9ca5b0a1efbec3d33d7cab03617
JLLQA <jlewis@qa.com>
2021-06-03 16:05:53 +0100

    JL: discussion hotfix

--- Cinema/qac/src/Components/Discussion/DiscussionComponents/ExtraDetails.jsx
+++ Cinema/qac/src/Components/Discussion/DiscussionComponents/ExtraDetails.jsx
@@ -1,3 +1,4 @@
+import Navbar from "../../Navbar/Navbar"
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button, Container, Row, Col, Nav
@@ -10,35 +11,35 @@ const ExtraDetails = () => {
     const filter = new Filter();
     return (
         <>
-        <Nav/>
-        <Container>
-            <Row>
-                <Col>
-                    <Card>
-                        <CardBody>
+            <Navbar />
+            <Container>
+                <Row>
+                    <Col>
+                        <Card>
+                            <CardBody>
                                 <CardTitle tag="h5">{filter.clean(dummydata.title)}</CardTitle>
                                 <CardSubtitle tag="h6" className="mb-2 text-muted">{filter.clean(dummydata.username)}</CardSubtitle>
                                 <CardText>{filter.clean(dummydata.body)}</CardText>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-            {dummydata.replies.map((reply) => (
-                <Row>
-                
-                <Col>
-                <Card>
-                <CardBody>
-                <CardTitle tag="h5">{filter.clean(reply.username)}</CardTitle>
-                <CardText>{filter.clean(reply.body)}</CardText>
-                </CardBody>
-                </Card>
-                </Col>
+                            </CardBody>
+                        </Card>
+                    </Col>
                 </Row>
-            ))}
+                {dummydata.replies.map((reply) => (
+                    <Row>
 
-        </Container>
-            </>
+                        <Col>
+                            <Card>
+                                <CardBody>
+                                    <CardTitle tag="h5">{filter.clean(reply.username)}</CardTitle>
+                                    <CardText>{filter.clean(reply.body)}</CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                ))}
+
+            </Container>
+        </>
     )
 }
 
