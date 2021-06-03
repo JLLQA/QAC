c9e3366bc0c2e3b5e970c966efe7743ad4b0acb4
JLLQA <jlewis@qa.com>
2021-06-03 17:08:30 +0100

    JL: initial backend

---
+++ Cinema/backend/models/index.js
@@ -0,0 +1,19 @@
+const MONGOOSE = require("mongoose");
+const EXPRESS = require("express");
+const APP = EXPRESS();
+
+// const routes = require("./routes");
+
+
+// setup mongoose
+MONGOOSE.connect("mongodb://localhost/movie", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        const APP = EXPRESS();
+        APP.use("/api", routes);
+        APP.listen(5000, () => {
+            console.log("Server has started");
+        })
+    })
