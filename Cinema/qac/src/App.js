75f4952e60d724904194b81b140e169d018e35bc
philipugona <PUgona@qa.com>
2021-06-03 16:01:31 +0100

    PU: Added final changes before merging to dev

--- Cinema/qac/src/App.js
+++ Cinema/qac/src/App.js
@@ -1,12 +1,10 @@
 import HomePage from "./Components/Homepage/HomePage"
 import './App.css';
-import ContactUs from "./Components/Contact/Contact";
 
 const App = () => {
   return (
     <>
     <HomePage />
-    <ContactUs />
     </>
   );
 }
