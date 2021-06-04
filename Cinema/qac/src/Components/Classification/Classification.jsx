5517bd3b24738282ce51f731918c7196388462b2
sajathraffeekQA <MMohamedRaffeek@qa.com>
2021-06-04 14:54:51 +0100

    QAC-40 #comment Saj: created classifications page

---
+++ Cinema/qac/src/Components/Classification/Classification.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../Multipage/Navbar/Navbar";
+
+const Classification =()=>{
+
+return(
+
+    <>
+      <Navbar />
+    <p>hello classification</p>
+
+</>
+);
+
+
+}
+export default Classification;
