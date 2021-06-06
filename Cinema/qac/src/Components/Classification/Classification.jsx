bdd30d3f27ffdc4ff20d253976bc7ecd9c9a93d1
sajathraffeekQA <MMohamedRaffeek@qa.com>
2021-06-07 00:27:56 +0100

    QAC-40 #comment Saj: created cards for each classification, including links and images

--- Cinema/qac/src/Components/Classification/Classification.jsx
+++ Cinema/qac/src/Components/Classification/Classification.jsx
@@ -1,16 +1,83 @@
 import Navbar from "../Multipage/Navbar/Navbar";
 
-const Classification =()=>{
+import React from 'react';
+import {
+  Card, CardImg, CardText,
+  CardTitle, Row, Col, Button
+} from 'reactstrap';
 
-return(
+const Classification = () => {
+    return (
+        <>
+            <Navbar />
+<h1>Classification</h1>
 
-    <>
-      <Navbar />
-    <p>hello classification</p>
+<div id="dropped-box" className="container-fluid">
+        <Row>
+      <Col sm="6">
+      <CardImg src="https://static.wikia.nocookie.net/rating-system/images/8/88/BBFC_U.svg" alt="U Universal - Suitable for all"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">U Universal - Suitable for all</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/u">Go somewhere</Button>
+        </Card>
+      </Col>
+      <Col sm="6">
+      <CardImg src="https://static.wikia.nocookie.net/rating-system/images/b/b5/BBFC_PG.svg" alt="PG Parental Guidance"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">PG Parental Guidance</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/pg">Go somewhere</Button>
+        </Card>
+      </Col>
+    
+      <Col sm="6">
+      <CardImg src="https://static.wikia.nocookie.net/rating-system/images/8/87/BBFC_12A.svg" alt="12A – Suitable for 12 years and over"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">12A – Suitable for 12 years and over</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/12">Go somewhere</Button>
+        </Card>
+      </Col>
+      <Col sm="6">
+      <CardImg src="https://static.wikia.nocookie.net/rating-system/images/8/84/BBFC_12.svg" alt="12 – Suitable for 12 years and over"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">12 – Suitable for 12 years and over</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/12">Go somewhere</Button>
+        </Card>
+      </Col>
+    
+      <Col sm="6">
+      <CardImg src="https://static.wikia.nocookie.net/rating-system/images/0/03/BBFC_15.svg" alt="15 – Suitable only for 15 years and over"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">15 – Suitable only for 15 years and over</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/15">Go somewhere</Button>
+        </Card>
+      </Col>
+      <Col sm="6">
+      <CardImg src="https://static.wikia.nocookie.net/rating-system/images/9/9f/BBFC_18.svg" alt="18 – Suitable only for adults"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">18 – Suitable only for adults</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/18">Go somewhere</Button>
+        </Card>
+      </Col>
+    
+    <Col sm="6">
+        <CardImg src="https://static.wikia.nocookie.net/rating-system/images/9/90/BBFC_R18.svg" alt="R18 - To be shown only in specially licensed cinemas, or supplied only in licensed sex shops, and to adults only"></CardImg>
+        <Card body>
+          <CardTitle tag="h5">R18 - To be shown only in specially licensed cinemas, or supplied only in licensed sex shops, and to adults only</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button href="https://www.bbfc.co.uk/rating/r18">Go somewhere</Button>
+        </Card>
+      </Col>
+    </Row>
 
-</>
-);
+    </div>
+        </>
+    );
+};
 
-
-}
 export default Classification;
