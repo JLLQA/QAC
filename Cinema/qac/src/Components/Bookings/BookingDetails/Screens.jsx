078030aba1db3da32a1700b8916bc60cbf41a76f
JLLQA <jlewis@qa.com>
2021-06-06 23:18:06 +0100

    QAC-42 JL: Seat pictures and screens open in new tab

--- Cinema/qac/src/Components/Bookings/BookingDetails/Screens.jsx
+++ Cinema/qac/src/Components/Bookings/BookingDetails/Screens.jsx
@@ -8,10 +8,14 @@ const Screens = () => {
             </div>
             <div id="dropped-box" className="container-fluid">
                 <Container>
-                    <h1>This is a standard screen</h1>
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/a/a1/Mallard2.jpg'></img>
-                    <h1>This is a deluxe screen</h1>
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/a/a1/Mallard2.jpg'></img>
+                    <div align="center">
+                        <h1>This is a standard screen</h1>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/a/a1/Mallard2.jpg' alt="duck"></img>
+                        <img src='https://c.stocksy.com/a/sS2400/z9/962790.jpg' alt="basicseat"></img>
+                        <h1>This is a deluxe screen</h1>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/a/a1/Mallard2.jpg' alt="duck2"></img>
+                        <img src='https://www.showcasecinemas.com/Media/1739/woburnseatsmob2.jpg' alt="deluxeseat"></img>
+                    </div>
                 </Container>
             </div>
         </div>
