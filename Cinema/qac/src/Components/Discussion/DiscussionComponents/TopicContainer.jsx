97ef500e3ecbd7ffb3b8ac4ddff6e0e212427e1d
markgasmidoQA <MGasmido@qa.com>
2021-06-08 14:17:23 +0100

    MG: fixed slight bug with search link - final completed need css

--- Cinema/qac/src/Components/Discussion/DiscussionComponents/TopicContainer.jsx
+++ Cinema/qac/src/Components/Discussion/DiscussionComponents/TopicContainer.jsx
@@ -22,7 +22,7 @@ const TopicContainer = (props) => {
                                         <CardTitle tag="h5">{filter.clean(post.title)}</CardTitle>
                                         <CardSubtitle tag="h6">{filter.clean(post.username)}</CardSubtitle>
                                         <CardText>{filter.clean(post.body)}</CardText>
-                                        <Link to={{ pathname: `/discussion/movie/${post.title}` }}>
+                                        <Link to={{ pathname: `/discussion/${post.title}` }}>
                                             <h3>More</h3>
                                         </Link>
                                         <br />
