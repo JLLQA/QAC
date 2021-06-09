34afa45695e17c0ef715f178fb6d00b2fe55a0b5
philipugona <PUgona@qa.com>
2021-06-09 17:42:23 +0100

    QAC-69 #comment PU: Finalized the testing of the New Releases Routes. Achieved over 90% coverage for this.

--- Cinema/backend/test/routeReleases-test.js
+++ Cinema/backend/test/routeReleases-test.js
@@ -25,10 +25,11 @@ after((done) => {
 });
 
 //New Releases request testing
-describe('/routeReleases Testing....', () => {
+describe('/routeReleases Testing for NewReleases....', () => {
 
     it('should verify that we have 0 movies in the DB', (done) => {
-        chai.request(server)
+        chai
+            .request(server)
             .get('/newreleases')
             .end((err, res) => {
                 res.should.have.status(200);
@@ -83,7 +84,8 @@ describe('/routeReleases Testing....', () => {
     });
 
     it('should verify that we have 4 movies in the DB', (done) => {
-        chai.request(server)
+        chai
+            .request(server)
             .get('/newReleases')
             .end((err, res) => {
                 res.should.have.status(200);
@@ -95,7 +97,8 @@ describe('/routeReleases Testing....', () => {
 
     //Test to get the ID for the candyman movie and deleteByID
     it('should verify that we have can deleteById', (done) => {
-        chai.request(server)
+        chai
+            .request(server)
             .get('/newReleases')
             .end((err, res) => {
                 res.should.have.status(200);
@@ -103,44 +106,47 @@ describe('/routeReleases Testing....', () => {
                 res.body[2].title.should.equal("Candyman");
                 let candyId = res.body[2]._id;
 
-                chai.request(server)
+                chai
+                .request(server)
                     .delete('/delById/' + candyId)
                     .end((err, res) => {
                         res.should.have.status(200);
                         console.log(res.body._id);
                         expect(candyId).to.equal(res.body._id);
-                done();
+                        done();
+                    });
             });
-        });
     });
 
     it('should verify that we have 3 movies in the DB after the delete', (done) => {
-        chai.request(server)
-        .get('/newReleases')
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            res.body.length.should.be.eql(3);
-            done();
-        });
+        chai
+            .request(server)
+            .get('/newReleases')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(3);
+                done();
+            });
     });
 
     it('should verify that we should get an error when attempting to delete a movie that does not exist', (done) => {
-        chai.request(server)
-        .get('/newReleases')
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            let randomId = 40;
+        chai
+            .request(server)
+            .get('/newReleases')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                let randomId = 40;
 
-            chai.request(server)
-                .delete('/delById/' + randomId)
-                .end((err, res) => {
-                    res.should.have.status(500);
-            done();
-        });
+                chai.request(server)
+                    .delete('/delById/' + randomId)
+                    .end((err, res) => {
+                        res.should.have.status(500);
+                        done();
+                    });
+            });
     });
-});
 
 });
 
