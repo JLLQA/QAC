270aebd9728052618a5973b46ab45cf8c4ae6ca1
sajathraffeekQA <MMohamedRaffeek@qa.com>
2021-06-09 23:15:46 +0100

    QAC-97 Saj: Completed feature testing places branch

--- Cinema/qac/src/__tests__/Places/Places.test.js
+++ Cinema/qac/src/__tests__/Places/Places.test.js
@@ -1,36 +1,21 @@
 import '@testing-library/jest-dom';
-import { getByText, render, screen } from '@testing-library/react';
-import { Button } from 'reactstrap';
+import { render, screen } from '@testing-library/react';
 import Places from '../../Components/Places/Places';
 
 describe('test for place to go page', () => {
 
-test('PLACES TO GO text is present', () => {
-  render( <Places /> );
-  const linkElement = screen.getByText("PLACES TO GO");
-  expect(linkElement).toBeInTheDocument();
-  });
+    test('PLACES TO GO heading text is present', () => {
+        render(<Places />);
+        const linkElement = screen.getByText("PLACES TO GO");
+        expect(linkElement).toBeInTheDocument();
+    });
 
-});
-  
-  // test('PLACES TO GO link is present', () => {
-      
-      //  render (<Button id="external" href="https://www.visitmanchester.com/things-to-see-and-do" target="_blank">For comprehensive information of places to visit in Manchester,click here.
-//     </Button>)
+    test("testing hrefs to contain links", () => {
+        render(<Places />);
+        expect(screen.getByRole('link', { name: /For comprehensive information of places to visit in Manchester,click here./i }).closest('a')).toHaveAttribute('href', 'https://www.visitmanchester.com/things-to-see-and-do');
+        expect(screen.getByRole('link', { name: /Visit the 'Printworks' website for more info/i }).closest('a')).toHaveAttribute('href', 'https://printworks-manchester.com/');
+        expect(screen.getByRole('link', { name: /Visit the 'Manchester Art Gallery' website for more info/i }).closest('a')).toHaveAttribute('href', 'https://manchesterartgallery.org/');
+        expect(screen.getByRole('link', { name: /Visit the 'Sea Life Manchester Aquarium' website for more info/i }).closest('a')).toHaveAttribute('href', 'https://www.visitsealife.com/manchester/');
 
-//     expect(screen.getByText('click here').closest('a')).toHaveAttribute('href', 'https://www.visitmanchester.com/things-to-see-and-do');
-//     // expect(getByText('visit in Manchester,click here').closest('a')).toHaveAttribute('href', 'https://www.visitmanchester.com/things-to-see-and-do')
-
-//     // expect(getByText("visit in Manchester,click here").href).toBe("https://www.visitmanchester.com/things-to-see-and-do")
-// //   expect(screen.getByRole('link')).toHaveAttribute('href', 'https://www.visitsealife.com/manchester/', 'https://printworks-manchester.com/', 'https://manchesterartgallery.org/', 'https://www.visitmanchester.com/things-to-see-and-do');
-// });
-
-
-describe('test for manchester', () => {
-
-    test("testing href", () => {
-        render( <Places /> );
-        expect(screen.getByRole('link', {name: /For comprehensive information of places to visit in Manchester,click here./i}).closest('a')).toHaveAttribute('href', 'https://www.visitmanchester.com/things-to-see-and-do');
-
-});
+    });
 });
