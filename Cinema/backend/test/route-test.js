8a2e2267016c1d0d684558b800b250539015be2f
philipugona <PUgona@qa.com>
2021-06-09 18:40:52 +0100

    QAC-69 #comment PU: Acheived 81% backend test coverage before pulling down from main branch

--- Cinema/backend/test/route-test.js
+++ Cinema/backend/test/route-test.js
@@ -1,3 +1,141 @@
-// console.log(process.env.NODE_ENV+" this is the test environment");
+process.env.NODE_ENV = 'test';
 
+const MOVIE = require("../models/movie")
 
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+
+const chaiHttp = require('chai-http');
+const server = require('../index');
+
+chai.use(chaiHttp);
+
+//Delete all movies in the db before the tests are run
+before((done) => {
+    MOVIE.deleteMany({}, (err) => { });
+    done();
+});
+
+//Delete all movies in the db before the tests are run
+after((done) => {
+    MOVIE.deleteMany({}, (err) => { });
+    done();
+});
+
+describe('/route Testing for Listings....', () => {
+
+    it('should verify that we have 0 movies in the DB', (done) => {
+        chai
+            .request(server)
+            .get('/movies')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+
+    it('should verify that a new movie can be created and can findById', (done) => {
+        let MOV = new MOVIE({
+            "id": 1,
+            "title": "Demon Slayer",
+            "director": "Gotouge",
+            "genre": "Quest",
+            "year": 2021,
+            "actors": [{ "name": "Tanjiro" }, { "name": "Inosuke" }, { "name": "Zenitsu" }],
+            "poster": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQbYpI4mYx8iImEc1TVNqY5iJFREmo7-sGK7PdtBztb2qtS0Pzb",
+            "synopsis": "A boy raised by boars who wears a boar's head and another boy who reveals his true power when he sleeps.",
+            "reviews": [{ "critic": "mark", "stars": "5" }, { "critic": "hater", "stars": "1" }]
+        });
+        chai
+            .request(server)
+            .post('/movies/create')
+            .send(MOV)
+            .end((err, res) => {
+                let movTitle = res.body.title;
+                let movId = res.body.id;
+                console.log("the movie id:" + movId + " and the title: " + movTitle)
+                res.should.have.status(200);
+                expect(movTitle).to.equal("Demon Slayer");
+
+                chai
+                    .request(server)
+                    .get('/movies/find/' + movId)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.title.should.equal("Demon Slayer");
+                        done();
+                    });
+            });
+    });
+
+    //The update  test
+    // it('should verify that an existing movie can be updated with a review', (done) => {
+
+    //     let movReview = { "critic": "Critic1", "stars": 1, "review": "Did not like this movie unfortunately" };
+    //     chai
+    //         .request(server)
+    //         .get('/movies')
+    //         .end((err, res) => {
+    //             let movId = res.body[0].id;
+    //             console.log("The movie ID is: " + movId);
+    //             res.should.have.status(200);
+    //             res.body.should.be.a('array');
+
+    //             // chai
+    //             //     .request(server)
+    //             //     .patch('/movies/review/:id')
+    //             //     .send({ id: parseInt(movId)}, {$push: {reviews:movReview}})
+    //             //     .end((err, res) => {
+    //             //         console.log("THis ");
+    //             //         console.log(res.body);
+    //             //         res.should.have.status(404);
+
+    //             //     });
+    //             done();
+    //         // });
+    // });
+
+    // The update fail test
+    it('should verify that an a review can be added', (done) => {
+
+        let movReview = { "critic": "Critic1", "stars": 1, "review": "Did not like this movie unfortunately" };
+        chai
+            .request(server)
+            .get('/movies')
+            .end((err, res) => {
+                let movId = res.body[0].id;
+                console.log("The movie ID is: " + movId);
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+
+                chai
+                    .request(server)
+                    .patch('/movies/review/:id')
+                    .send({ id: parseInt(movId)}, {$push: {reviews:movReview}})
+                    .end((err, res) => {
+                        res.should.have.status(202);
+                        done();
+
+                    });
+            });
+    });
+
+    //Find by id for non existabnt id
+    it('should verify that you cannot find by a non existant id', (done) => {
+        chai
+        .request(server)
+        .get('/movies/find/' + 5)
+        .end((err, res) => {
+            // res.should.have.status(200);
+            // res.body.should.be.a('object');
+            console.log(res.body);
+            // res.body.title.should.equal("Demon Slayer");
+            done();
+        });
+
+    });
+});
---
+++ Cinema/backend/test/routeTopics-test.js
@@ -0,0 +1,113 @@
+process.env.NODE_ENV = 'test';
+
+const TOPIC = require("../models/Topics");
+
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+
+const chaiHttp = require('chai-http');
+const server = require('../index');
+const { findByIdAndUpdate } = require("../models/Topics");
+
+chai.use(chaiHttp);
+
+//Delete all movies in the db before the tests are run
+before((done) => {
+    TOPIC.deleteMany({}, (err) => { });
+    done();
+});
+
+//Delete all movies in the db before the tests are run
+after((done) => {
+    TOPIC.deleteMany({}, (err) => { });
+    done();
+});
+
+describe('/routeReleases Testing for NewReleases....', () => {
+
+    it('should verify that we have 0 topics in the DB', (done) => {
+        chai
+            .request(server)
+            .get('/topics')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+
+
+    //Test to create and find by id and title
+    it('should verify that we can create a topic and getByTitle', (done) => {
+
+        let movTopic = new TOPIC({
+            username: "Name1",
+            body: "This movie is interesting. You should watch",
+            title: "007 Movie",
+        });
+        chai
+            .request(server)
+            .post('/movies/topics/create')
+            .send(movTopic)
+            .end((err, res) => {
+                let movTitle = res.body.title;
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                console.log(res.body);
+                res.body.title.should.equal("007 Movie");
+                res.body.username.should.equal("Name1");
+
+                chai
+                    .request(server)
+                    .get('/topics/' + movTitle)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        expect(res.body.body).to.contain("This movie is interesting")
+                        done();
+                    });
+            });
+    });
+
+    // Can update an existing movie with a comment
+    it('should verify that an existing movie can be updated with a review', (done) => {
+
+        let movTop = {"username":"Ice", "body": "This is test review"};
+        chai
+            .request(server)
+            .get('/topics')
+            .end((err, res) => {
+                let movTitle = res.body[0].title;
+                console.log("The movie ID is: " + movTitle);
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+
+                chai
+                    .request(server)
+                    .patch('/movies/topics/comment/:title')
+                    .send({ title: movTitle}, {$push: {comments : movTop}} )
+                    .end((err, res) => {
+                        res.should.have.status(202);
+                        console.log(res.body);
+                        done();
+                    });
+                });
+    });
+
+    // it('should verify that we get an error when getting topic does not exist', (done) => {
+    //     let invalidTitle = "InvalidTitle";
+
+    //     chai
+    //         .request(server)
+    //         .get('/topics/' + invalidTitle)
+    //         .end((err, res) => {
+    //             // res.should.have.status(404);
+    //             // console.log(res);
+    //             // expect(res.body).to.contain("Error occured")
+    //             done();
+    //         });
+    // });
+
+
+});
