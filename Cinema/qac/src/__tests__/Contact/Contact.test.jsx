dd77341a23405eb3926e80d5d1a8772992768a8d
sajathraffeekQA <MMohamedRaffeek@qa.com>
2021-06-10 15:16:02 +0100

    QAC-99: #comment Saj: Completed testing for contact page

--- Cinema/qac/src/__tests__/Contact/Contact.test.jsx
+++ Cinema/qac/src/__tests__/Contact/Contact.test.jsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent,getByText,render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 
 import Contact from '../../Components/Contact/Contact';
 
@@ -9,5 +10,42 @@ describe('test for contact page', () => {
         render(<Contact />);
         const linkElement = screen.getByText("CONTACT");
         expect(linkElement).toBeInTheDocument();
+    });
+
+    test("testing hrefs to contain links", () => {
+        render(<Contact />);
+        expect(screen.getByRole('link', { name: /Click for more Travel Info/i }).closest('a')).toHaveAttribute('href', 'http://localhost:3000/gettingthere');
+        expect(screen.getByRole('button', { name: /Reset Form/i })).toHaveAttribute('type', 'reset');
+    });
+
+    test("It matches the snapshot", () => {
+        const tree = renderer.create(<Contact />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    test("testing the form", () => {
+
+        const handleSubmit = jest.fn();
+      const formTest = render(
+            <Contact handleSubmit={handleSubmit} />
+        );
+        
+        const nameInput = screen.getByPlaceholderText('Enter name here');
+        const emailInput = screen.getByPlaceholderText('Enter email here');
+              
+        const messageInput = screen.getByPlaceholderText('Enter message here');
+        
+        fireEvent.change(nameInput, { target: { value: "abc" } });
+        fireEvent.change(emailInput, { target: { value: "abc@qa.com" } });
+        
+        fireEvent.click(screen.getByText("-- Select a Subject --"));
+        fireEvent.click(screen.getByText("Movies"));
+        
+        fireEvent.change(messageInput, { target: { value: "hello" } });
+
+        expect(nameInput.value).toBe("abc");
+        expect(emailInput.value).toBe("abc@qa.com");
+        expect(messageInput.value).toBe("hello");
+           
     });
 });
